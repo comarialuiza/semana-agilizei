5fcf07b8a07aa65b8e0c05bb8bbc60eb23d8d807
Maria Luiza <co.maria.luiza@gmail.com>
2020-12-10 20:58:02 -0300

    Continues with second class

--- cypress/integration/purchase.js
+++ cypress/integration/purchase.js
@@ -15,5 +15,20 @@ context('Purchase', () => {
             .children('a')
             .first()
             .click()
+
+        cy.get('.button-container a[href$="controller=order"]').click();
+
+        cy.get('.cart_navigation a[href$="order&step=1"]').click();
+
+        cy.get('#email').type('teste@react.com.br');
+        cy.get('#passwd').type('test12345');
+        cy.get('button#SubmitLogin').click();
+
+        // cy.get('#addressesAreEqual')
+
+        cy.get('button[name=processAddress]').click();
+
+        cy.get('[type=checkbox]#cgv').check();
+        cy.get('button[name=processCarrier]').click();
     });
 });
